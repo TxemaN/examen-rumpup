cbceab74a960359f50c110a21e9fe79771f7a078
TxemaN <txalcala@hotmail.com>
2023-06-22 11:08:16 +0200

    ahora sí

--- prueba.js
+++ prueba.js
@@ -1,29 +1,11 @@
-let nota =0;
-let array=[];
-let notaMedia;
-let notaFinal;
+let letra = "n";
+let nuevaPalabra;
+let cantidadVeces;
+let frase = "assassins";
+let totalS;
 
-const pedirNota=()=> {
-
-  for(let i =1; i<4; i++)
-  {
-    let nota=Number(prompt("escribe la"+i+"ª nota"));
-    array.push(nota);
-    
-}
-notaMedia = array.reduce((primeranota, restonotas)=>(primeranota+restonotas))/3;
-
-
-  if (notaMedia <5 ) {
-    notaFinal="suspenso";
+function contarS()  {
+  nuevaPalabra= frase.split(letra);
+return nuevaPalabra.length - 1;
     }
-    else if (notaMedia>=5 && notaMedia<7) {
-      notaFinal="notable";
-    }
-    else if (notaMedia>=7 && notaMedia<=10) {
-      notaFinal="sobresaliente";
-    }
-    return notaFinal;
- }
-
-console.log(pedirNota())
+console.log(contarS());
