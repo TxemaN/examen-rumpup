ea8db5e69fdd3db5380f76de970196332d455483
TxemaN <txalcala@hotmail.com>
2023-06-22 11:21:55 +0200

    prompt para letra y frase

--- prueba.js
+++ prueba.js
@@ -5,6 +5,9 @@ let frase = "perros y peras";
 let totalS;
 
 function contarS()  {
+    letra=prompt("escribe la letra a contar"); if(letra.length>1) {
+        letra=prompt("solo una letra porfa")
+    }
     frase=prompt("escribe algo");
   nuevaPalabra= frase.split(letra);
 return nuevaPalabra.length - 1;
